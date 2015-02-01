c718ad0b8559512d4dea2c714f9fbf0eca4875c7
KalahariKanga <djfhill@mailinator.com>
2015-02-01 17:15:48 +0000

    Gain parameter for audiocomponents

--- AudioComponent.cpp
+++ AudioComponent.cpp
@@ -6,7 +6,9 @@ AudioComponent::AudioComponent()
 {
 	startn = n;
 	output.create(0, SHRT_MIN, SHRT_MAX, 1);
+	gain.create(0, -1, 1);
 	parameters.insert(std::pair<std::string, Parameter*>("output", &output));
+	parameters.insert(std::pair<std::string, Parameter*>("gain", &gain));
 }
 
 
@@ -31,6 +33,11 @@ void AudioComponent::update()
 	render();
 	float v = (buffer[0] + buffer[BUFFER_LENGTH / 3] + buffer[2 * BUFFER_LENGTH / 3]) / 3;
 	output.setBaseValue(v);
+
+	/*this could be incorporated into a component::getSample(int c) function*/
+	for (int c = 0; c < BUFFER_LENGTH; c++)
+		buffer.data[c] += buffer.data[c] * gain.getValue();
+
 	needUpdate = 0;
 }
 
